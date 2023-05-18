3db80f50097a74e3c8629e584d268da31c5b139b
Thyssenkrupp234 <Thyssenkrupp234@gmail.com>
2023-05-17 20:13:17 -0700

    delete spaces

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow} from "electron";
 import * as path from "path";
 
 function createWindow() {
@@ -37,6 +37,5 @@ app.on("window-all-closed", () => {
     app.quit();
   }
 });
-
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
